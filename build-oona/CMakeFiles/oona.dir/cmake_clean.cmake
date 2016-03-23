b98b1187bec40aa4b76260863e0bddc9ad43d353
ikarimov <ikarimov@simtechdev.com>
2016-03-23 06:33:05 +0300

    First commit

---
+++ build-oona/CMakeFiles/oona.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/oona.dir/main.cpp.o"
+  "oona.pdb"
+  "oona"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/oona.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
